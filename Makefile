0defdcc650222c40ca0bc880d2fc2191916fe99d
ael-bagh <ael-bagh@student.1337.ma>
2021-06-09 19:08:13 +0100

    header and make file

---
+++ Makefile
@@ -0,0 +1,24 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ael-bagh <ael-bagh@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/06/09 18:49:01 by ael-bagh          #+#    #+#              #
+#    Updated: 2021/06/09 18:52:05 by ael-bagh         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRC = execute.c ft_split.c main.c parsing_utils.c utils.c
+NAME = pipex
+
+all: $(NAME)
+
+$(NAME): 
+	gcc -Wall -Werror -Wextra -o pipex $(SRC)
+clean:
+	rm -f *.o
+fclean: clean
+	rm -f $(NAME)
+re: fclean all
--- pipex.h
+++ pipex.h
@@ -6,29 +6,43 @@
 /*   By: ael-bagh <ael-bagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 13:39:02 by ael-bagh          #+#    #+#             */
-/*   Updated: 2021/06/09 15:49:41 by ael-bagh         ###   ########.fr       */
+/*   Updated: 2021/06/09 19:01:02 by ael-bagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
+#ifndef PIPEX_H
 
-typedef struct  s_cmd
+# define PIPEX_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <errno.h>
+# include <fcntl.h>
+
+typedef struct s_cmd
 {
-    char    **cmd_w_args;
-    char    *cmd_abs_path;
-}               t_cmd;
+	char	**cmd_w_args;
+	char	*cmd_abs_path;
+}				t_cmd;
 
-char	*ft_strdup(const char *s1);
-char	**ft_split(char const *s, char c);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlen(const char *s);
-void    free_the_nipples(char **s);
-void    execute_it(t_cmd cmd[2], char *infile, char *outfile, char **envp);
-int     c_finder(const char *str, char c);
-int     ft_strcmp(const char *str1, const char *str2);
+typedef struct s_plit
+{
+	int	start;
+	int	len;
+}
+				t_plit;
+
+void		init(int *k, int *i);
+char		*ft_strdup(const char *s1);
+char		**ft_split(char const *s, char c);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+size_t		ft_strlen(const char *s);
+void		free_the_nipples(char **s);
+void		execute_it(t_cmd cmd[2], char *infile, char *outfile, char **envp);
+int			c_finder(const char *str, char c);
+int			ft_strcmp(const char *str1, const char *str2);
+int			arg_error(t_cmd cmd[2]);
+
+#endif
